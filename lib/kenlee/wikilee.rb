faa6bea416b77d4abc2111083f2257e4407c587d
Luka Murn <luka.murn@biosistemika.com>
2016-05-26 15:48:24 +0200

    Rename paragraph->extract, add links

--- lib/kenlee/wikilee.rb
+++ lib/kenlee/wikilee.rb
@@ -5,9 +5,9 @@ module KenLee
 
     MAX_PAGE_LIMIT = 10
     ENDPOINT_PREFIX = 'https://{{lang}}.wikipedia.org/w/api.php'
-    INTRO_TEMPLATE =
+    MAIN_TEMPLATE =
     '{{ep}}?action=query&generator=random&{{rnc}}&grnlimit={{limit}}&' +
-    'grnnamespace=0&prop=extracts&exlimit={{limit}}&exintro&explaintext&' +
+    'grnnamespace=0&prop={{prop}}&{{prop_params}}&' +
     'format=json'
 
     attr_reader :endpoint
@@ -15,38 +15,88 @@ module KenLee
     def initialize(lang = :en)
       @endpoint = ENDPOINT_PREFIX.gsub('{{lang}}', lang.to_s)
       @rnc = nil
-      @paragraphs = []
+      @extracts = []
+      @links = []
+      @links_continue = nil
     end
 
-    def paragraph
-      fetch_paragraph
+    def extract
+      get_single_extract
     end
 
-    def paragraphs(nr = 10)
-      (1..nr).collect { fetch_paragraph }
+    def extracts(nr = 10)
+      (1..nr).collect { get_single_extract }
+    end
+
+    def link
+      get_single_link
+    end
+
+    def links(nr = 10)
+      (1..nr).collect { get_single_link }
     end
 
     private
 
-    def fetch_paragraph
-      if @paragraphs.empty? then
-        query_paragraphs
+    def get_data_json(prop, prop_params = nil)
+      if prop_params.nil? || prop_params.empty?
+        prop_params_str = ''
+      else
+        prop_params_str = prop_params.collect{|k,v| v.nil? ? "#{k}" : "#{k}=#{v}"}.join('&')
       end
-      @paragraphs.pop
-    end
 
-    def query_paragraphs
-      url = INTRO_TEMPLATE
+      url = MAIN_TEMPLATE
       url = url.gsub('{{ep}}', @endpoint)
       url = url.gsub('{{rnc}}', @rnc.nil? ? '' : "grncontinue=#{@rnc}")
       url = url.gsub('{{limit}}', "#{MAX_PAGE_LIMIT}")
+      url = url.gsub('{{prop}}', prop)
+      url = url.gsub('{{prop_params}}', prop_params_str)
       http = Curl.get(url)
-      json = JSON.parse(http.body_str)
+      JSON.parse(http.body_str)
+    end
+
+    def get_single_extract
+      if @extracts.empty? then
+        fetch_extracts
+      end
+      @extracts.pop
+    end
+
+    def fetch_extracts
+      json = get_data_json('extracts', { 'exlimit' => MAX_PAGE_LIMIT, 'exintro' => nil, 'explaintext' => nil })
 
       @rnc = (json.has_key? "continue") ? json["continue"]["grncontinue"] : nil
       if json.has_key? "query"
         json["query"]["pages"].values.each do |page|
-          @paragraphs.push(page["extract"])
+          @extracts.push(page["extract"])
+        end
+      else
+        raise Exception, ""
+      end
+    end
+
+    def get_single_link
+      if @links.empty? then
+        fetch_links
+      end
+      @links.pop
+    end
+
+    def fetch_links
+      prop_params = { 'ellimit' => MAX_PAGE_LIMIT }
+      unless @links_continue.nil?
+        prop_params["eloffset"] = @links_continue
+      end
+      json = get_data_json('extlinks', prop_params)
+
+      @links_continue = (json.has_key? "continue") ? json["continue"]["eloffset"] : nil
+      if json.has_key? "query"
+        json["query"]["pages"].values.each do |page|
+          if page.has_key? "extlinks"
+            page["extlinks"].each do |link_map|
+              @links.push(link_map["*"])
+            end
+          end
         end
       else
         raise Exception # TODO
--- test/test_kenlee_wikilee.rb
+++ test/test_kenlee_wikilee.rb
@@ -10,25 +10,44 @@ class TestKenLeeWikiLee < Test::Unit::TestCase
     assert @wl.endpoint.start_with? 'https://en.wikipedia.org/w/api.php'
   end
 
-  def test_paragraph
-    paragraph = @wl.paragraph
-    assert_paragraph(paragraph)
+  def test_extract
+    extract = @wl.extract
+    assert_extract(extract)
   end
 
-  def test_paragraphs
-    paragraphs = @wl.paragraphs(15)
-    assert !paragraphs.nil?
-    assert paragraphs.is_a? Array
-    paragraphs.each do |paragraph|
-      assert_paragraph(paragraph)
+  def test_extracts
+    extracts = @wl.extracts(15)
+    assert !extracts.nil?
+    assert extracts.is_a? Array
+    extracts.each do |extract|
+      assert_extract(extract)
+    end
+  end
+
+  def test_link
+    link = @wl.link
+    assert_link(link)
+  end
+
+  def test_links
+    links = @wl.links(15)
+    assert !links.nil?
+    assert links.is_a? Array
+    links.each do |link|
+      assert_link(link)
     end
   end
 
   private
 
-  def assert_paragraph(paragraph)
-    assert !paragraph.nil?
-    assert paragraph.is_a? String
+  def assert_extract(extract)
+    assert !extract.nil?
+    assert extract.is_a? String
+  end
+
+  def assert_link(link)
+    assert !link.nil?
+    assert link.is_a? String
   end
 
 end
